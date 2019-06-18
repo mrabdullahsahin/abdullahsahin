fe9b8cbe08e2ada4dccff7ad1fb09f2cb01f670b
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-18 14:27:54 +0300

    Add makerlog iframe to my website

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,6 +35,14 @@ const IndexPage = () => (
 
       </div>
     </div>
+
+    <div className="Logs">
+      <h2>My Logs</h2>
+      <div className="MakerLog">
+        <iframe title="Makerlog Embed" height="300" width="75%" align="middle" scrolling="no" frameborder="0" allowtransparency="true" src="https://api.getmakerlog.com/users/2864/embed"></iframe>
+      </div>
+    </div>
+
     <SEO title="Home"/>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
