e451061375fd442114a1bce57c3cf24f0464a774
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-20 17:21:05 +0300

    Make HeroArea component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,9 +3,11 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Header from "../components/IndexHeroArea/IndexHeroArea"
 
 const SecondPage = () => (
   <Layout>
+    <Header/>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
