32ce9569a50f90c6ef2703e83aa6e561b05029af
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-24 14:17:28 +0300

    🌟 Make Yearly Goal Page Folder

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -5,6 +5,7 @@ exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---src-pages-page-2-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-yearly-goal-page-yearlygoal-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/Yearly Goal Page/yearlygoal.js" /* webpackChunkName: "component---src-pages-yearly-goal-page-yearlygoal-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/page-2.js")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/page-2.js"))),
+  "component---src-pages-yearly-goal-page-yearlygoal-js": hot(preferDefault(require("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/Yearly Goal Page/yearlygoal.js")))
 }
 
