b5208a08e3b00ed8e6d604b43dcb989d82401a79
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-25 14:06:20 +0300

    🚧 Add about page

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---src-pages-404-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-page-2-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
-  "component---src-pages-yearlygoal-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/yearlygoal.js" /* webpackChunkName: "component---src-pages-yearlygoal-js" */)
+  "component---src-pages-yearlygoal-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/yearlygoal.js" /* webpackChunkName: "component---src-pages-yearlygoal-js" */),
+  "component---src-pages-about-js": () => import("/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */)
 }
 
