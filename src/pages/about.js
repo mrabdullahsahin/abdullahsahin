9a06d3aef6050c4c3716e1735797e208dd0c8c4f
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-25 14:09:22 +0300

    🚧 Add other component to about page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./yearlygoal.css"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroArea from "../components/IndexHeroArea/IndexHeroArea"
+import Footer from "../components/Footer/Footer"
+
+const About = () => (
+    <Layout>
+        <HeroArea/>
+        <SEO title="About"/>
+        
+        <Footer/>
+    </Layout>
+)
+
+export default About
