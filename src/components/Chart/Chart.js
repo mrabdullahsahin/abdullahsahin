4e40cb4c2e6bb5601ac1acde0b7c5e6052349eda
abdullahsahin <abdullahsahinceo@gmail.com>
2019-06-27 08:13:29 +0300

    📊 Update Chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Line} from 'react-chartjs-2'
+
+class Chart extends Component{
+    render(){
+      return (
+        <div>
+          TEST
+        </div>
+      )
+    }
+  }
+  
+  export default Chart;
