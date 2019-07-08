48239a83fbb819a8a718cd29a0670f83c699bab5
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-08 14:15:00 +0300

    Add to static graphql query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { graphql, useStaticQuery} from 'gatsby'
 
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    console.log(data)
+
+
     return (
         <Layout>
             <h1>Blog</h1>
