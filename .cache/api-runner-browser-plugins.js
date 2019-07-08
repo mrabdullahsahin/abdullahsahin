ecb1a385ac06c35a6960e4a6496aeda32fc33292
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-08 16:39:38 +0300

    Update to gatsby config file

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":750,"linkImagesToOriginal":false},
+    },{
       plugin: require('/Users/abdullah/Desktop/Github/mrabdullahsahin.github.io/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
---
+++ src/posts/post2/test.md
@@ -0,0 +1,14 @@
+---
+title: "Test25 Post"
+date: "2019-04-04"
+---
+
+I just launched a new post.
+
+![Black](./black.jpg)
+
+## Topic Covered
+
+1. Test1
+2. Test2
+3. Test3
