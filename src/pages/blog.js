4595cb0f592a4cee93ae375560866823dca4e29f
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-09 14:25:18 +0300

    Update to Blog page with Head component and SEO

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { graphql, useStaticQuery, Link} from 'gatsby'
 
 import Layout from '../components/layout'
+import Head from "../components/Head"
+import SEO from "../components/seo"
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -24,6 +26,8 @@ const BlogPage = () => {
 
     return (
         <Layout>
+        <Head title="Blog"/>
+        <SEO title="Blog"/>
             <h1>Blog</h1>
             <ol>
                 {data.allMarkdownRemark.edges.map((edge) => {
