b73a855d31753a29224b405de607d89dd888b347
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-10 14:52:06 +0300

    Update to LatestPost component

--- src/components/LatestPost/LatestPost.js
+++ src/components/LatestPost/LatestPost.js
@@ -1,27 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import './LatestPost.css'
 
-class LatestPost extends React.Component {
-    render () {
-        return(
-            <div className="Posts">
-                <h2> Latest Post </h2>
-                <div className="PostsGroup">
-                    <Link to="/" className="Post">
-                        <h1> My Test Post </h1>
-                        <p> Jun 22, 2019 </p>
-                    </Link>
-                    <hr/>
-                    <Link to="/" className="Post">
-                        <h1> My Post </h1>
-                        <p> Jun 21, 2019 </p>
-                    </Link>
-                </div>
-          </div>
-        )
-    }
+const LatestPost = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    return (
+        <div className="Posts">
+            <h2> Latest Post </h2>
+            <div className="PostsGroup">
+                {data.allMarkdownRemark.edges.map((edge) => {
+                        return(
+                            <li>
+                                <Link to={`/blog/${edge.node.fields.slug}`} className="Post">
+                                    <h1>{edge.node.frontmatter.title}</h1>
+                                    <p>{edge.node.frontmatter.date}</p>
+                                </Link>
+                                <hr></hr>
+                            </li>
+                        )
+                    })}
+            </div>
+        </div>
+    )
 }
 
 export default LatestPost
