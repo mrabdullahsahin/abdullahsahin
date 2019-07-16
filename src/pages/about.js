ed13f68d3e38239d69767484dc29b7a0fe2af1e2
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-16 07:19:23 +0300

    Update the About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -31,6 +31,7 @@ const AboutText = styled.p`
     text-align: justify;
     font-size: 25px;
     width: 75%;
+    line-height: 1.5;
     @media (max-width: 640px){
         font-size: 20px;
     }
@@ -44,20 +45,39 @@ const About = () => (
         <AboutArea>
             <AboutTitle>About</AboutTitle>
             <AboutText>
-                Hi, I'm Abdullah from Turkey. I'm a software engineer, digital craftsman and indie maker. If you wondering about my projects and me, You can contact me with the social accounts below.
+                Hi, 
+                <br></br>
+                <br></br>
+                My name is <strong>Abdullah</strong> and I'm a Software Engineer, Digital Craftsman and Indie Maker. I’m mainly interested in <strong>SaaS</strong>, <strong>UI</strong>, <strong>UX</strong>, <strong>Python</strong> (programming language)! Here, in my personal website you’ll find my musings and thoughts about random topics. Some of the my social media accounts:
 
                 <ul>
-                    <li> <a href="https://github.com/mrabdullahsahin"> Github </a> </li>
-                    <li> <a href="https://www.linkedin.com/in/mrabdullahsahin/"> Linkedin </a></li>
-                    <li> <a href="https://twitter.com/mrabdullahsahin"> Twitter </a></li>
-                    <li> <a href="https://www.instagram.com/mrabdullahsahin/"> Instagram </a></li>
-                    <li> <a href="https://www.goodreads.com/user/show/78190102-abdullah"> Goodreads </a></li>
-                    <li> <a href="https://dribbble.com/abdullahsahin"> Dribbble </a></li>
-                    <li> <a href="https://www.behance.net/abdullahsaf7fe/"> Behance </a></li>
-                    <li> <a href="https://getmakerlog.com/@abdullahsahin"> Makerlog </a></li>
-                    <li> <a href="https://medium.com/@abdullahsahin"> Medium </a></li>
-                    <li> <a href="https://www.kaggle.com/abdullahsahin"> Kaggle </a></li>
+                    <li> <a href="https://github.com/mrabdullahsahin" target="blank"> Github </a> </li>
+                    <li> <a href="https://www.linkedin.com/in/mrabdullahsahin/" target="blank"> Linkedin </a></li>
+                    <li> <a href="https://twitter.com/mrabdullahsahin" target="blank"> Twitter </a></li>
+                    <li> <a href="https://www.instagram.com/mrabdullahsahin/" target="blank"> Instagram </a></li>
+                    <li> <a href="https://www.goodreads.com/user/show/78190102-abdullah" target="blank"> Goodreads </a></li>
+                    <li> <a href="https://dribbble.com/abdullahsahin" target="blank"> Dribbble </a></li>
+                    <li> <a href="https://www.behance.net/abdullahsaf7fe/" target="blank"> Behance </a></li>
+                    <li> <a href="https://getmakerlog.com/@abdullahsahin" target="blank"> Makerlog </a></li>
+                    <li> <a href="https://medium.com/@abdullahsahin" target="blank"> Medium </a></li>
+                    <li> <a href="https://www.kaggle.com/abdullahsahin" target="blank"> Kaggle </a></li>
                 </ul>
+
+                Some of the open source projects I’ve wrote in the past or currently maintaining are:
+
+                <ul>
+                    <li> <a href="https://github.com/mrabdullahsahin/data-structures-algorithms" target="blank"> data-structures-algorithms </a> </li>
+                    <li> <a href="https://github.com/mrabdullahsahin/data-analysis-data-visualization" target="blank"> data-analysis-data-visualization </a> </li>
+                    <li> <a href="https://github.com/mrabdullahsahin/fikrin-hayati" target="blank"> fikrin-hayati </a> </li>
+                    <li> <a href="https://github.com/mrabdullahsahin/yolharitasi" target="blank"> yolharitasi </a> </li>
+                    <li> <a href="https://github.com/mrabdullahsahin/ios-programlama-kaynaklar" target="blank"> ios-programlama-kaynaklar </a> </li>
+                    <li>… and many more on <a href="https://github.com/mrabdullahsahin" target="blank"> my personal Github profile! </a> </li>
+                </ul>
+
+                For random bits follow me on twitter: <strong><a href="https://twitter.com/mrabdullahsahin" target="blank">@mrabdullahsahin</a> </strong>. For more info about my professional career, visit my <strong><a href="https://www.linkedin.com/in/mrabdullahsahin/" target="blank">Linkedin profile</a></strong>.
+                <br></br>
+                <br></br>
+                If you have any questions please feel free to send an email to: <strong> abdullahsahinceo @ gmail.com </strong>
 
             </AboutText>
         </AboutArea>
