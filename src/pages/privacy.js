976a78314d0937ca586c2e370ff878fadf3f9f1c
abdullahsahin <abdullahsahinceo@gmail.com>
2019-07-19 06:26:33 +0300

    Fix the Privacy Policy page text mistake

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -45,7 +45,7 @@ const Privacy = () => (
         <PrivacyArea>
             <PrivacyTitle>Privacy Policy</PrivacyTitle>
             <PrivacyText>
-                We do not share persona≈l information with third-parties nor do we store information we collect about your visit to this blog for use other than to analyze content performance through the use of cookies, which you can turn off at anytime by modifying your internet browser's settings. We are not responsible for the republishing of the content found on this blog on other websites or media without our permission. This privacy policy is subject to change without notice.
+                We do not share personal information with third-parties nor do we store information we collect about your visit to this blog for use other than to analyze content performance through the use of cookies, which you can turn off at anytime by modifying your internet browser's settings. We are not responsible for the republishing of the content found on this blog on other websites or media without our permission. This privacy policy is subject to change without notice.
             </PrivacyText>
         </PrivacyArea>
 
